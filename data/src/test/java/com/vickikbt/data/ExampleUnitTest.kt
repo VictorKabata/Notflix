709bd78ea76e5dd156ea40e7934dd2b6f43ced44
VictorKabata <39780120+VictorKabata@users.noreply.github.com>
2021-04-28 13:51:30 +0300

    Added test libs to app and data module and in action

--- data/src/test/java/com/vickikbt/data/ExampleUnitTest.kt
+++ data/src/test/java/com/vickikbt/data/ExampleUnitTest.kt
@@ -1,14 +1,9 @@
 package com.vickikbt.data
 
+import org.junit.Assert.*
 import org.junit.Test
 
-import org.junit.Assert.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
