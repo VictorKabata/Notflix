18e01bf8625f30c2eb3ef1ad74f234fff8e28e09
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2021-12-31 05:38:28 +0300

    feat: Complete setup up of main activity

--- app/src/main/java/com/vickikbt/notflix/ui/navigation/Navigation.kt
+++ app/src/main/java/com/vickikbt/notflix/ui/navigation/Navigation.kt
@@ -1,7 +1,6 @@
 package com.vickikbt.notflix.ui.navigation
 
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
@@ -19,25 +18,25 @@ import com.vickikbt.notflix.ui.screens.settings.SettingsScreen
 @Composable
 fun Navigation(navController: NavHostController) {
 
-    val defaultEnterAnimationDuration = 600
-    val defaultExitAnimationDuration = 1100
-    val slideDefaultInitialOffset = 1800
-    val slideDefaultTargetOffset = 1500
+    //val defaultEnterAnimationDuration = 600
+    //val defaultExitAnimationDuration = 1100
+    //val slideDefaultInitialOffset = 1800
+    //val slideDefaultTargetOffset = 1500
 
-    AnimatedNavHost(navController = navController, startDestination = NavigationItem.Home.route){
-        composable(route=NavigationItem.Home.route){
+    AnimatedNavHost(navController = navController, startDestination = NavigationItem.Home.route) {
+        composable(route = NavigationItem.Home.route) {
             HomeScreen(navController = navController)
         }
 
-        composable(route=NavigationItem.Favorites.route){
+        composable(route = NavigationItem.Favorites.route) {
             FavoritesScreen(navController = navController)
         }
 
-        composable(route=NavigationItem.Settings.route){
+        composable(route = NavigationItem.Settings.route) {
             SettingsScreen(navController = navController)
         }
 
-        composable(route=NavigationItem.Details.route){
+        composable(route = NavigationItem.Details.route) {
             DetailsScreen(navController = navController)
         }
     }
