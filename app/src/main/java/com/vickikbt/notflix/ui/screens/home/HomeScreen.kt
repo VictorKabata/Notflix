d3bc9f2b1cdbe10f9bdd6249eca69df60f07f940
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-01-02 17:37:47 +0300

    feat: Completed item upcoming movies UI

--- app/src/main/java/com/vickikbt/notflix/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/vickikbt/notflix/ui/screens/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.vickikbt.notflix.ui.screens.home
 
+<<<<<<< Updated upstream
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
@@ -9,6 +10,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+=======
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+>>>>>>> Stashed changes
 import androidx.navigation.NavController
 
 @Composable
@@ -24,6 +35,26 @@ fun HomeScreen(navController: NavController) {
     }
 }
 
+<<<<<<< Updated upstream
 fun crash() {
     throw RuntimeException("Test Crash")
 }
+=======
+    val scrollState = rememberScrollState()
+
+    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.surface){
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(scrollState)) {
+
+        }
+
+    }
+
+}
+
+@Composable
+fun UpcomingMovies(navController: NavController){
+
+}
+>>>>>>> Stashed changes
