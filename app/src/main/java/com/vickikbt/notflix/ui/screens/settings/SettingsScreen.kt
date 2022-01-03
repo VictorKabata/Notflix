35011840d8c7dad857c92bc86fb240544828be94
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-01-03 16:18:01 +0300

    feat: Created preferences composable components

--- app/src/main/java/com/vickikbt/notflix/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/vickikbt/notflix/ui/screens/settings/SettingsScreen.kt
@@ -1,8 +1,30 @@
 package com.vickikbt.notflix.ui.screens.settings
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.vickikbt.notflix.R
+import com.vickikbt.notflix.ui.components.AppBar
 
 @Composable
 fun SettingsScreen(navController: NavController) {
+
+    Scaffold(topBar = { AppBar(stringResource(id = R.string.title_settings)) }) {
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.surface) {
+            Column {
+                Spacer(modifier = Modifier.height(8.dp))
+
+            }
+        }
+    }
+
 }
