424f342623b4371c80ab4360624419cf6cb37ef3
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-01-03 17:22:16 +0300

    feat: Added toolbar in favorites screen

--- app/src/main/java/com/vickikbt/notflix/ui/screens/favorites/FavoritesScreen.kt
+++ app/src/main/java/com/vickikbt/notflix/ui/screens/favorites/FavoritesScreen.kt
@@ -1,9 +1,24 @@
 package com.vickikbt.notflix.ui.screens.favorites
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
+import com.vickikbt.notflix.R
+import com.vickikbt.notflix.ui.components.AppBar
 
 @Composable
-fun FavoritesScreen(navController: NavController){
+fun FavoritesScreen(navController: NavController) {
+
+    Scaffold(topBar = { AppBar(stringResource(id = R.string.title_favorites)) }) {
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.surface) {
+
+
+        }
+    }
 
 }
