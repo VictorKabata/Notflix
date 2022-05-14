1c5a47b0059b126de225ae60cb2eac823a6025b5
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-05-14 03:10:48 +0300

    Created movie details dao

---
+++ shared/src/commonMain/kotlin/com/vickikbt/shared/data/cache/sqldelight/MovieDetailsDao.kt
@@ -0,0 +1,26 @@
+package com.vickikbt.shared.data.cache.sqldelight
+
+import com.squareup.sqldelight.runtime.coroutines.asFlow
+import com.vickikbt.shared.domain.utils.DatabaseDriverFactory
+import kotlinx.coroutines.flow.map
+
+class MovieDetailsDao constructor(private val databaseDriverFactory: DatabaseDriverFactory) {
+
+    private val appDatabase = AppDatabase(databaseDriverFactory.createDriver())
+    private val dbQuery = appDatabase.appDatabaseQueries
+
+    fun saveMovieDetails(movieDetailsEntity: MovieDetailsEntity) {
+        dbQuery.transaction {
+            dbQuery.saveMovieDetails(movieDetailsEntity)
+        }
+    }
+
+    fun getMovieDetails(movieId: Int) =
+        dbQuery.getMovieDetails(id = movieId).asFlow().map { it.executeAsOneOrNull() }
+
+    fun deleteMovieDetails() = dbQuery.transaction {
+        dbQuery.deleteAllMovieDetais()
+    }
+
+    fun isMovieDetailsAvailable(movieId: Int) = dbQuery.isMovieDetailsAvailable(id = movieId)
+}
