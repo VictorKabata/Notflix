86429ef0fff8be3a6c16abe79ebc9a15ace63e7d
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-05-22 15:05:17 +0300

    Renamed async image class

--- desktopApp/src/main/kotlin/utils/AsyncImage.kt
+++ desktopApp/src/main/kotlin/utils/AsyncImage.kt
@@ -14,41 +14,6 @@ import kotlinx.coroutines.withContext
 import java.io.IOException
 import java.net.URL
 
-/*@Composable
-fun koil(url: String): ImageBitmap? {
-    var image by remember(url) { mutableStateOf<ImageBitmap?>(null) }
-
-    LaunchedEffect(url) {
-        loadFullImage(url)?.let {
-            image = Image.makeFromEncoded(toByteArray(it)).toComposeImageBitmap()
-        }
-    }
-
-    return image
-}
-
-fun loadFullImage(source: String): BufferedImage? {
-    return try {
-        val url = URL(source)
-        val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
-        connection.connectTimeout = 5000
-        connection.connect()
-
-        val input: InputStream = connection.inputStream
-        val bitmap: BufferedImage? = ImageIO.read(input)
-        bitmap
-    } catch (e: Exception) {
-        e.printStackTrace()
-        null
-    }
-}
-
-fun toByteArray(bitmap: BufferedImage): ByteArray {
-    val baos = ByteArrayOutputStream()
-    ImageIO.write(bitmap, "png", baos)
-    return baos.toByteArray()
-}*/
-
 @Composable
 fun <T> AsyncImage(
     load: suspend () -> T,
