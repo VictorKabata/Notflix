0faa5fd036853d025699c295c1b6d92e97ca1161
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-06-06 22:35:53 +0300

    ktLint and detekt integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,30 +29,30 @@ allprojects {
 }
 
 subprojects {
-//    apply(plugin = Plugins.ktLint)
-//    ktlint {
-//        debug.set(true)
-//        verbose.set(true)
-//        android.set(false)
-//        outputToConsole.set(true)
-//        outputColorName.set("RED")
-//        filter {
-//            exclude("**/generated/**")
-//            include("**/kotlin/**")
-//        }
-//    }
-//
-//    apply(plugin = Plugins.detekt)
-//    detekt {
-//        parallel = true
-//        config = files("${project.rootDir}/config/detekt/detekt.yml")
-//    }
-//
-//    tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
-//        checkForGradleUpdate = true
-//        outputDir = "build/dependencyUpdates"
-//        reportfileName = "report"
-//    }
+    apply(plugin = Plugins.ktLint)
+    ktlint {
+        debug.set(true)
+        verbose.set(true)
+        android.set(false)
+        outputToConsole.set(true)
+        outputColorName.set("RED")
+        filter {
+            exclude("**/generated/**")
+            include("**/kotlin/**")
+        }
+    }
+
+    apply(plugin = Plugins.detekt)
+    detekt {
+        parallel = true
+        config = files("${project.rootDir}/config/detekt/detekt.yml")
+    }
+
+    tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
+        checkForGradleUpdate = true
+        outputDir = "build/dependencyUpdates"
+        reportfileName = "report"
+    }
 }
 
 tasks.register("clean").configure {
