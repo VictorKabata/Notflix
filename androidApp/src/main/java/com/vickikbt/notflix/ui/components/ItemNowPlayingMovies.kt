3772d400ffe0b1e37d1b598b870d152a8b298c12
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-06-06 22:45:00 +0300

    Fixed linting issues

--- androidApp/src/main/java/com/vickikbt/notflix/ui/components/ItemNowPlayingMovies.kt
+++ androidApp/src/main/java/com/vickikbt/notflix/ui/components/ItemNowPlayingMovies.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
@@ -141,9 +140,4 @@ fun ItemNowPlayingMovies(
             //endregion
         }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun Preview() {
 }
