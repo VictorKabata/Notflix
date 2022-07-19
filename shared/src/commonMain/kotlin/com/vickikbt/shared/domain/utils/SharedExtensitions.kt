4450b968d47700a2945577dd520d79ea67468bab
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-07-19 23:45:22 +0300

    Fix trending movies endpoint

--- shared/src/commonMain/kotlin/com/vickikbt/shared/domain/utils/SharedExtensitions.kt
+++ shared/src/commonMain/kotlin/com/vickikbt/shared/domain/utils/SharedExtensitions.kt
@@ -1,4 +1,24 @@
 package com.vickikbt.shared.domain.utils
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import org.koin.core.component.KoinComponent
+
 fun String.capitalizeWords(): String =
     split(" ").joinToString(" ") { it.replaceFirstChar { if (it.isLowerCase()) it.titlecase() else it.toString() } }
+
+fun KoinComponent.viewModelScope(operation: suspend () -> Unit) {
+    val viewModelScope = CoroutineScope(Dispatchers.Default)
+    val supervisorJob = MutableStateFlow<Job?>(null)
+
+    val job = viewModelScope.launch { operation.invoke() }
+
+    supervisorJob.value = job
+    job.invokeOnCompletion {
+        supervisorJob.value?.cancel()
+        supervisorJob.value = null
+    }
+}
