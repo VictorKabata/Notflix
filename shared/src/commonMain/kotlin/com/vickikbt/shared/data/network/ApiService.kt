45c21cf4efb53684bd094084dc9a174db0388627
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-08-16 22:56:09 +0300

    Implemented safeApiCall in repo layer

--- shared/src/commonMain/kotlin/com/vickikbt/shared/data/network/ApiService.kt
+++ shared/src/commonMain/kotlin/com/vickikbt/shared/data/network/ApiService.kt
@@ -10,14 +10,14 @@ import io.ktor.client.request.*
 
 class ApiService constructor(private val httpClient: HttpClient) {
 
-    suspend fun fetchNowPlayingMovies(page: Int = STARTING_PAGE_INDEX): MovieResultsDto? {
+    suspend fun fetchNowPlayingMovies(page: Int = STARTING_PAGE_INDEX): MovieResultsDto {
         return httpClient.get<MovieResultsDto>(urlString = "movie/now_playing") {
             parameter("page", page)
         }
 
     }
 
-    suspend fun fetchPopularMovies(page: Int = STARTING_PAGE_INDEX): MovieResultsDto? {
+    suspend fun fetchPopularMovies(page: Int = STARTING_PAGE_INDEX): MovieResultsDto {
         return httpClient.get<MovieResultsDto>(urlString = "movie/popular") {
             parameter("page", page)
         }
@@ -28,31 +28,31 @@ class ApiService constructor(private val httpClient: HttpClient) {
         mediaType: String = "movie",
         timeWindow: String = "week",
         page: Int = STARTING_PAGE_INDEX
-    ): MovieResultsDto? {
+    ): MovieResultsDto {
         return httpClient.get<MovieResultsDto>(urlString = "trending/$mediaType/$timeWindow") {
             parameter("page", page)
         }
     }
 
-    suspend fun fetchUpcomingMovies(page: Int = STARTING_PAGE_INDEX): MovieResultsDto? {
+    suspend fun fetchUpcomingMovies(page: Int = STARTING_PAGE_INDEX): MovieResultsDto {
         return httpClient.get<MovieResultsDto>(urlString = "movie/upcoming") {
             parameter("page", page)
         }
     }
 
-    suspend fun fetchMovieDetails(movieId: Int): MovieDetailsDto? {
+    suspend fun fetchMovieDetails(movieId: Int): MovieDetailsDto {
         return httpClient.get<MovieDetailsDto>(urlString = "movie/$movieId") {
             parameter("movie_id", movieId)
         }
     }
 
-    suspend fun fetchMovieCast(movieId: Int): CastDto? {
+    suspend fun fetchMovieCast(movieId: Int): CastDto {
         return httpClient.get<CastDto>(urlString = "movie/$movieId/credits") {
             parameter("movie_id", movieId)
         }
     }
 
-    suspend fun fetchMovieVideos(movieId: Int): MovieVideoDto? {
+    suspend fun fetchMovieVideos(movieId: Int): MovieVideoDto {
         return httpClient.get<MovieVideoDto>(urlString = "movie/$movieId/videos") {
             parameter("movie_id", movieId)
         }
@@ -61,7 +61,7 @@ class ApiService constructor(private val httpClient: HttpClient) {
     suspend fun fetchSimilarMovies(
         movieId: Int,
         page: Int = STARTING_PAGE_INDEX
-    ): MovieResultsDto? {
+    ): MovieResultsDto {
         return httpClient.get<MovieResultsDto>(urlString = "movie/$movieId/similar") {
             parameter("page", page)
         }
