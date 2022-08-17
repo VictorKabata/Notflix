5ce01a713da092e9d1c4739aaef64282dae602b4
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-08-18 00:16:47 +0300

    Create PaletteGenerator.kt

---
+++ androidApp/src/main/java/com/vickikbt/notflix/util/PaletteGenerator.kt
@@ -0,0 +1,24 @@
+package com.vickikbt.notflix.util
+
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import androidx.palette.graphics.Palette
+
+class PaletteGenerator {
+    companion object{
+        fun generateImagePalette(imageDrawable: Drawable, onGenerated: (Palette.Swatch) -> Unit) {
+            val bitmap = (imageDrawable as BitmapDrawable).bitmap.copy(Bitmap.Config.ARGB_8888, true)
+            Palette.from(bitmap).maximumColorCount(24).generate { palette ->
+                val vibrantSwatch = palette?.vibrantSwatch
+                val dominantSwatch = palette?.vibrantSwatch
+
+                if (vibrantSwatch != null) {
+                    onGenerated(vibrantSwatch)
+                } else if (dominantSwatch != null) {
+                    onGenerated(dominantSwatch)
+                }
+            }
+        }
+    }
+}
