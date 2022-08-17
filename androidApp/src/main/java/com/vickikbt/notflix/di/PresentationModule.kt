66e06c07c8c6d478a6f6cab53208c6eb478d7505
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-08-18 01:13:14 +0300

    removed homeviewmodel koin viewmodel

--- androidApp/src/main/java/com/vickikbt/notflix/di/PresentationModule.kt
+++ androidApp/src/main/java/com/vickikbt/notflix/di/PresentationModule.kt
@@ -1,7 +1,6 @@
 package com.vickikbt.notflix.di
 
 import com.vickikbt.notflix.ui.activity.MainViewModel
-import com.vickikbt.notflix.ui.screens.home.HomeViewModel
 import com.vickikbt.notflix.util.LocaleManager
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -10,6 +9,5 @@ val presentationModule = module {
 
     single { LocaleManager() }
 
-    viewModel { HomeViewModel() }
     viewModel { MainViewModel(get()) }
 }
