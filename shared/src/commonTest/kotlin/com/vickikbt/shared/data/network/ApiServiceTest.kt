58a3ab20fffb3b7e01d8a6ae70818bc146dd359b
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-08-17 12:20:15 +0300

    Added some ktor client mock tests

--- shared/src/commonTest/kotlin/com/vickikbt/shared/data/network/ApiServiceTest.kt
+++ shared/src/commonTest/kotlin/com/vickikbt/shared/data/network/ApiServiceTest.kt
@@ -1,37 +1,55 @@
 package com.vickikbt.shared.data.network
 
-import com.vickikbt.shared.SampleData.Companion.MOCK_ERROR_RESPONSE
 import com.vickikbt.shared.SampleData.Companion.MOCK_MOVIE_RESPONSE
-import com.vickikbt.shared.data.network.MockServer.Companion.expectSuccess
 import com.vickikbt.shared.data.network.MockServer.Companion.mockHttpClient
-import com.vickikbt.shared.data.network.models.ApiError
 import com.vickikbt.shared.data.network.models.MovieResultsDto
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
-import kotlin.test.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 
 class ApiServiceTest {
 
     // Subject under test
     private val apiService = ApiService(httpClient = mockHttpClient)
 
-    @BeforeTest
-    fun setup() {
-
-    }
-
-    @AfterTest
-    fun tearDown() {
-        // mockHttpClient.close()
-    }
-
     @Test
     fun `fetch now playing movies success returns movies`() = runBlocking {
         val networkResponse = apiService.fetchNowPlayingMovies()
         val expectedResponse = Json.decodeFromString<MovieResultsDto>(MOCK_MOVIE_RESPONSE)
 
+        assertNotNull(actual = networkResponse)
         assertEquals(expected = expectedResponse, actual = networkResponse)
     }
+
+    @Test
+    fun `fetch popular movies success returns movies`() = runBlocking {
+        val networkResponse = apiService.fetchPopularMovies()
+        val expectedResponse = Json.decodeFromString<MovieResultsDto>(MOCK_MOVIE_RESPONSE)
+
+        assertNotNull(actual = networkResponse)
+        assertEquals(expected = expectedResponse, actual = networkResponse)
+    }
+
+    @Test
+    fun `fetch trending movies success returns movies`() = runBlocking {
+        val networkResponse = apiService.fetchTrendingMovies()
+        val expectedResponse = Json.decodeFromString<MovieResultsDto>(MOCK_MOVIE_RESPONSE)
+
+        assertNotNull(actual = networkResponse)
+        assertEquals(expected = expectedResponse, actual = networkResponse)
+    }
+
+    @Test
+    fun `fetch upcoming movies success returns movies`() = runBlocking {
+        val networkResponse = apiService.fetchUpcomingMovies()
+        val expectedResponse = Json.decodeFromString<MovieResultsDto>(MOCK_MOVIE_RESPONSE)
+
+        assertNotNull(actual = networkResponse)
+        assertEquals(expected = expectedResponse, actual = networkResponse)
+    }
+
 
 }
