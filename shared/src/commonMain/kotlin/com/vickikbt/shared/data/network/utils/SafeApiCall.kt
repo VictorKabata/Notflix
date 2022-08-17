7327734e56a5f40b4e262d92500e2485262e2951
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-08-17 12:10:41 +0300

    Added api error check

--- shared/src/commonMain/kotlin/com/vickikbt/shared/data/network/utils/SafeApiCall.kt
+++ shared/src/commonMain/kotlin/com/vickikbt/shared/data/network/utils/SafeApiCall.kt
@@ -1,24 +1,33 @@
 package com.vickikbt.shared.data.network.utils
 
+import com.vickikbt.shared.data.network.models.ApiError
 import io.ktor.client.features.*
+import io.ktor.utils.io.*
+import kotlinx.serialization.decodeFromString
 
 suspend fun <T : Any> safeApiCall(apiCall: suspend () -> T): NetworkResult<T> {
     return try {
         NetworkResult.Success(data = apiCall.invoke())
     } catch (e: RedirectResponseException) { // 3xx errors
+        val networkError = getError(responseContent = e.response.content)
+
         NetworkResult.Error(
-            errorCode = e.response.status.value,
-            errorMessage = e.message
+            errorCode = networkError.statusCode ?: e.response.status.value,
+            errorMessage = networkError.statusMessage ?: e.message
         )
     } catch (e: ClientRequestException) { // 4xx errors
+        val networkError = getError(responseContent = e.response.content)
+
         NetworkResult.Error(
-            errorCode = e.response.status.value,
-            errorMessage = e.message
+            errorCode = networkError.statusCode ?: e.response.status.value,
+            errorMessage = networkError.statusMessage ?: e.message
         )
     } catch (e: ServerResponseException) { // 5xx errors
+        val networkError = getError(responseContent = e.response.content)
+
         NetworkResult.Error(
-            errorCode = e.response.status.value,
-            errorMessage = e.message
+            errorCode = networkError.statusCode ?: e.response.status.value,
+            errorMessage = networkError.statusMessage ?: e.message
         )
     } catch (e: Exception) {
         NetworkResult.Error(
@@ -26,4 +35,11 @@ suspend fun <T : Any> safeApiCall(apiCall: suspend () -> T): NetworkResult<T> {
             errorMessage = e.message ?: "An unknown error occurred"
         )
     }
+}
+
+suspend fun getError(responseContent: ByteReadChannel): ApiError {
+    responseContent.readUTF8Line()?.let {
+        return kotlinx.serialization.json.Json.decodeFromString(string = it)
+    }
+    throw IllegalArgumentException("Not a parsable error")
 }
