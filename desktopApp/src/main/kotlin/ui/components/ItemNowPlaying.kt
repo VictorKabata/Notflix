d296b4a8466f821a2b90a7bc4bd4ac8e4563899a
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-09-15 19:19:34 +0300

    Change trending movies UI

--- desktopApp/src/main/kotlin/ui/components/ItemNowPlaying.kt
+++ desktopApp/src/main/kotlin/ui/components/ItemNowPlaying.kt
@@ -1,17 +1,24 @@
 package ui.components
 
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.onPointerEvent
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -23,15 +30,27 @@ import utils.AsyncImage
 import utils.loadImage
 import utils.loadImageBitmap
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun ItemNowPlayingMovies(
     modifier: Modifier = Modifier,
     movie: Movie,
     onItemClick: (Movie) -> Unit
 ) {
-    val imageUrl = movie.backdropPath?.loadImage()
+    var isHovered by remember { mutableStateOf(false) }
 
-    Box(modifier = modifier.fillMaxWidth().clickable { onItemClick(movie) }) {
+    val boxWidth by animateDpAsState(targetValue = if (isHovered) 400.dp else 200.dp)
+
+    val imageUrl = (if (isHovered) movie.backdropPath else movie.posterPath)?.loadImage()
+
+    Box(
+        modifier = modifier.width(boxWidth)
+            .clickable { onItemClick(movie) }
+            .onPointerEvent(PointerEventType.Enter) { isHovered = true }
+            .onPointerEvent(PointerEventType.Exit) { isHovered = false },
+    ) {
+
+
         AsyncImage(
             modifier = Modifier.fillMaxSize(),
             load = { loadImageBitmap(imageUrl) },
@@ -41,17 +60,19 @@ fun ItemNowPlayingMovies(
         )
 
         //region Movie Title
-        Text(
-            modifier = Modifier.padding(horizontal = 8.dp, vertical = 16.dp)
-                .align(Alignment.BottomStart),
-            text = movie.title ?: "Unknown Movie",
-            fontSize = 48.sp,
-            maxLines = 2,
-            fontWeight = FontWeight.Bold,
-            overflow = TextOverflow.Ellipsis,
-            textAlign = TextAlign.Start,
-            color = MaterialTheme.colors.onSurface
-        )
+        if (isHovered) {
+            Text(
+                modifier = Modifier.padding(horizontal = 8.dp, vertical = 16.dp)
+                    .align(Alignment.BottomStart),
+                text = movie.title ?: "Unknown Movie",
+                fontSize = 48.sp,
+                maxLines = 2,
+                fontWeight = FontWeight.Bold,
+                overflow = TextOverflow.Ellipsis,
+                textAlign = TextAlign.Start,
+                color = MaterialTheme.colors.onSurface
+            )
+        }
         //endregion
     }
 }
