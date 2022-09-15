f2250260a3e6f30ba3622783924779958e3e9e17
ndiritumichael <ndiritumichael5@gmail.com>
2022-09-15 14:29:31 +0300

    create iosProject

--- iOSNotflix/Notflix/ContentView.swift
+++ iOSNotflix/Notflix/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
         VStack {
--- shared/src/commonMain/kotlin/com/vickikbt/shared/di/Koin.kt
+++ shared/src/commonMain/kotlin/com/vickikbt/shared/di/Koin.kt
@@ -1,5 +1,11 @@
 package com.vickikbt.shared.di
 
+import com.vickikbt.shared.presentation.presenters.SharedDetailsPresenter
+import com.vickikbt.shared.presentation.presenters.SharedFavouritesPresenter
+import com.vickikbt.shared.presentation.presenters.SharedHomePresenter
+import com.vickikbt.shared.presentation.presenters.SharedSettingsPresenter
+import org.koin.core.Koin
+import org.koin.core.KoinApplication
 import org.koin.core.context.startKoin
 import org.koin.dsl.KoinAppDeclaration
 
@@ -11,3 +17,16 @@ fun initKoin(enableNetworkLogs: Boolean = false, appDeclaration: KoinAppDeclarat
 
 // called by iOS etc
 // fun initKoin() = initKoin(enableNetworkLogs = false) {}
+
+fun KoinApplication.Companion.start(): KoinApplication = initKoin { }
+
+val Koin.homePresenter: SharedHomePresenter
+    get() = get()
+val Koin.mainPresenter: SharedHomePresenter
+    get() = get()
+val Koin.detailsPresenter: SharedDetailsPresenter
+    get() = get()
+val Koin.settingsPresenter: SharedSettingsPresenter
+    get() = get()
+val Koin.favoritesPresenter: SharedFavouritesPresenter
+    get() = get()
