c9865d698b583f78e71841506a440e32cf229bc9
ndiritumichael <ndiritumichael5@gmail.com>
2022-09-30 17:13:52 +0300

    implement Movie detail screen

---
+++ iOSNotflix/Notflix/UI/Features/DetailsScreen/DetailsViewModel.swift
@@ -0,0 +1,72 @@
+//
+//  DetailsViewModel.swift
+//  Notflix
+//
+//  Created by Michael Ndiritu on 20/09/2022.
+//
+
+import Foundation
+import shared
+import KMPNativeCoroutinesAsync
+
+@MainActor
+class DetailsViewModel : ObservableObject {
+    @LazyKoin
+  private  var presenter : SharedDetailsPresenter
+    
+    @Published private (set) var movieDetails: MovieDetails? = nil
+    
+    @Published private (set) var movieCast : Cast? = nil
+    @Published private (set) var similarMovies = [Movie]()
+    @Published private (set) var movieVideo : MovieVideo?
+    
+    func observeMovieDetails(for movieId : Int ){
+        
+        presenter.getMovieDetails(movieId: Int32( movieId))
+        let _ = Task {
+            
+            do{
+                let result = asyncStream(for: presenter.movieDetailsNative)
+                for try await details in result{
+                    movieDetails = details
+                }
+            }
+            
+        }
+    }
+    func observeMovieCast(for movieId : Int){
+        presenter.getMovieCast(movieId: Int32(movieId))
+        Task{
+            do{
+                let castStream  = asyncStream(for: presenter.movieCastNative)
+                for try await cast in castStream{
+                    movieCast = cast
+                }
+            }
+        }
+    }
+    
+    
+    func observeSimilarMovies(for movieId: Int){
+        presenter.fetchSimilarMovies(movieId: Int32(movieId))
+        Task{
+            let similarMovieStream  = asyncStream(for: presenter.similarMoviesNative)
+            
+            
+            for try await  movies in similarMovieStream{
+                if let movies  {
+                    similarMovies = movies
+                }
+            }
+        }
+    }
+    
+    
+    func toggleFavorites(movieId : Int ,isFav : Bool){
+        print("saving to favorite")
+       // presenter.updateFavorite(cacheId: <#T##Int32#>, isFavorite: <#T##Bool#>)
+    }
+    
+    
+    
+}
