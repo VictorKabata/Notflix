e2b90d3dcbd649c9b7c86e0d35f1f7e015b83110
ndiritumichael <ndiritumichael5@gmail.com>
2022-09-30 17:03:08 +0300

    Explicitly declared ktor engines

--- shared/src/commonMain/kotlin/com/vickikbt/shared/di/CommonModule.kt
+++ shared/src/commonMain/kotlin/com/vickikbt/shared/di/CommonModule.kt
@@ -38,7 +38,7 @@ fun commonModule(enableNetworkLogs: Boolean) = module {
      * API client via constructor injection
      */
     single {
-        HttpClient {
+        HttpClient (get()){
             defaultRequest {
                 url {
                     host = BASE_URL
--- shared/src/jvmMain/kotlin/com/vickikbt/shared/di/Module.kt
+++ shared/src/jvmMain/kotlin/com/vickikbt/shared/di/Module.kt
@@ -1,9 +1,11 @@
 package com.vickikbt.shared.di
 
 import com.vickikbt.shared.domain.utils.MultiplatformSettingsWrapper
+import io.ktor.client.engine.java.*
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 actual fun platformModule(): Module = module {
     single { MultiplatformSettingsWrapper().createSettings() }
+    single { Java.create() }
 }
