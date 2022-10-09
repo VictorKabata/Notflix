5a36648a9b8b6890bdff32e2320abc3504b6a0f0
ndiritumichael <ndiritumichael5@gmail.com>
2022-10-09 22:02:11 +0300

    update views to be within the safe area.

--- iOSNotflix/Notflix/UI/Features/Home/HomeScreen.swift
+++ iOSNotflix/Notflix/UI/Features/Home/HomeScreen.swift
@@ -12,35 +12,39 @@ struct HomeScreen: View {
     @StateObject var viewModel = HomeViewModel()
     let navigatetoDetails : (Int) -> Void
     var body: some View {
+        
+        VStack{
+            Rectangle().frame(height: 0.01)
         ScrollView {
             LazyVStack(spacing: 10){
                 
-            
                 
-               
+                
+                
                 NowPlaying(nowPlayingMovies: viewModel.nowPlayingMovies,onClick: navigatetoDetails)
-                 
-            
+                
+                
                 TrendingMovies(movies: viewModel.trendingMovies,onClick: navigatetoDetails).padding(.leading,10).padding(.top,10)
                 
                 PopularMovies(movies: viewModel.popularMovies,onClick: navigatetoDetails).padding(.leading,10).padding(.top,10)
                 
                 TrendingMovies(header: "Upcoming Movies" ,movies: viewModel.upcomingMovies,onClick: navigatetoDetails).padding(.leading,10).padding(.top,10)
-            
-                    
+                
+                
                 
             }
         }
-            .refreshable {
-                Task{
-                    viewModel.observeContent()
-                }
-            }.alert("Something Went Wrong", isPresented: $viewModel.showAlert) {
-                Button("OK", role: .cancel) {
-                    viewModel.resetError()
-                    viewModel.observeContent()
-                }
+        .refreshable {
+            Task{
+                viewModel.observeContent()
             }
+        }.alert("Something Went Wrong", isPresented: $viewModel.showAlert) {
+            Button("OK", role: .cancel) {
+                viewModel.resetError()
+                viewModel.observeContent()
+            }
+        }//.frame(height: UIScreen.screenHeight)
+        }
     }
 }
 
