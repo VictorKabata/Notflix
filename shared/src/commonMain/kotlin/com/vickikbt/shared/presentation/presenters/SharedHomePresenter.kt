85b5f362f8c8722bcd31575e06c01730a269d3b8
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-10-13 18:28:13 +0300

    Clean up

--- shared/src/commonMain/kotlin/com/vickikbt/shared/presentation/presenters/SharedHomePresenter.kt
+++ shared/src/commonMain/kotlin/com/vickikbt/shared/presentation/presenters/SharedHomePresenter.kt
@@ -9,7 +9,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.cache
 import kotlinx.coroutines.launch
 import org.koin.core.component.KoinComponent
 
