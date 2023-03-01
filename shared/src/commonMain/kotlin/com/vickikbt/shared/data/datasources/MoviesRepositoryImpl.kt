a7e0531b95a9565bd64233eca82ef7c2261f66b8
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2023-03-01 13:02:58 +0300

    Replace category string with MovieCategories enums

--- shared/src/commonMain/kotlin/com/vickikbt/shared/data/datasources/MoviesRepositoryImpl.kt
+++ shared/src/commonMain/kotlin/com/vickikbt/shared/data/datasources/MoviesRepositoryImpl.kt
@@ -16,30 +16,31 @@ class MoviesRepositoryImpl constructor(
     private val moviesDao: MovieDao
 ) : MoviesRepository {
 
-    override suspend fun fetchMovies(category: String): Flow<Result<List<Movie>>> = safeApiCall {
-        val networkResponse = when (category) {
-            Enums.MovieCategories.NOW_PLAYING.name -> {
-                apiService.fetchNowPlayingMovies().movies.take(5)
+    override suspend fun fetchMovies(category: Enums.MovieCategories): Flow<Result<List<Movie>>> =
+        safeApiCall {
+            val networkResponse = when (category) {
+                Enums.MovieCategories.NOW_PLAYING -> {
+                    apiService.fetchNowPlayingMovies().movies.take(5)
+                }
+                Enums.MovieCategories.UPCOMING -> {
+                    apiService.fetchUpcomingMovies().movies
+                }
+                Enums.MovieCategories.POPULAR -> {
+                    apiService.fetchPopularMovies().movies
+                }
+                else -> {
+                    apiService.fetchTrendingMovies().movies.filter { it.mediaType == "movie" }
+                }
             }
-            Enums.MovieCategories.UPCOMING.name -> {
-                apiService.fetchUpcomingMovies().movies
-            }
-            Enums.MovieCategories.POPULAR.name -> {
-                apiService.fetchPopularMovies().movies
-            }
-            else -> {
-                apiService.fetchTrendingMovies().movies.filter { it.mediaType == "movie" }
-            }
-        }
 
-        return@safeApiCall networkResponse.map { it.toDomain() }
-    }
+            return@safeApiCall networkResponse.map { it.toDomain() }
+        }
 
     override suspend fun getMovies(category: String): Flow<List<Movie>> {
         val cachedResponse = moviesDao.getMoviesByCategory(category = category)
             .map { it.map { movieEntity -> movieEntity.toDomain(category = category) } }
             .onEach { movies ->
-                if (movies.isEmpty()) fetchMovies(category = category)
+                // if (movies.isEmpty()) fetchMovies(category = category)
             }
 
         return cachedResponse
--- shared/src/commonMain/kotlin/com/vickikbt/shared/domain/repositories/MoviesRepository.kt
+++ shared/src/commonMain/kotlin/com/vickikbt/shared/domain/repositories/MoviesRepository.kt
@@ -1,12 +1,13 @@
 package com.vickikbt.shared.domain.repositories
 
 import com.vickikbt.shared.domain.models.Movie
+import com.vickikbt.shared.domain.utils.Enums
 import kotlinx.coroutines.flow.Flow
 
 interface MoviesRepository {
 
     /** Fetch movies based on category from network*/
-    suspend fun fetchMovies(category: String): Flow<Result<List<Movie>>>
+    suspend fun fetchMovies(category: Enums.MovieCategories): Flow<Result<List<Movie>>>
 
     /** Get movies based on category from cache*/
     @Deprecated("Pending caching implementation")
