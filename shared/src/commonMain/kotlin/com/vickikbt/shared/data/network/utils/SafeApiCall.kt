ff52e5a0135aff3abe48165dc84d5289bd1d4439
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2023-03-01 12:59:20 +0300

    Returning flow from safeApiCall

--- shared/src/commonMain/kotlin/com/vickikbt/shared/data/network/utils/SafeApiCall.kt
+++ shared/src/commonMain/kotlin/com/vickikbt/shared/data/network/utils/SafeApiCall.kt
@@ -5,24 +5,28 @@ import io.ktor.client.features.RedirectResponseException
 import io.ktor.client.features.ServerResponseException
 import io.ktor.client.statement.HttpResponse
 import io.ktor.util.network.UnresolvedAddressException
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
-internal suspend fun <T : Any> safeApiCall(apiCall: suspend () -> T): Result<T> = try {
-    Result.success(apiCall.invoke())
-} catch (e: RedirectResponseException) {
-    val error = parseNetworkError(e.response)
-    Result.failure(exception = error)
-} catch (e: ClientRequestException) {
-    val error = parseNetworkError(e.response)
-    Result.failure(exception = error)
-} catch (e: ServerResponseException) {
-    val error = parseNetworkError(e.response)
-    Result.failure(exception = error)
-} catch (e: UnresolvedAddressException) {
-    val error = parseNetworkError(exception = e)
-    Result.failure(exception = error)
-} catch (e: Exception) {
-    val error = parseNetworkError(exception = e)
-    Result.failure(exception = error)
+internal suspend fun <T : Any> safeApiCall(apiCall: suspend () -> T): Flow<Result<T>> = flow {
+    try {
+        emit(Result.success(apiCall.invoke()))
+    } catch (e: RedirectResponseException) {
+        val error = parseNetworkError(e.response)
+        emit(Result.failure(exception = error))
+    } catch (e: ClientRequestException) {
+        val error = parseNetworkError(e.response)
+        emit(Result.failure(exception = error))
+    } catch (e: ServerResponseException) {
+        val error = parseNetworkError(e.response)
+        emit(Result.failure(exception = error))
+    } catch (e: UnresolvedAddressException) {
+        val error = parseNetworkError(exception = e)
+        emit(Result.failure(exception = error))
+    } catch (e: Exception) {
+        val error = parseNetworkError(exception = e)
+        emit(Result.failure(exception = error))
+    }
 }
 
 /**Generate [Exception] from network or system error when making network calls
