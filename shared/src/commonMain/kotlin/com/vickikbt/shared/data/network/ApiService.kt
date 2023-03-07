dfb837f5929023d489377847c8b29471c57fdc3d
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2023-03-07 13:58:31 +0300

    Quick fix to api service class
    Added full url

--- shared/src/commonMain/kotlin/com/vickikbt/shared/data/network/ApiService.kt
+++ shared/src/commonMain/kotlin/com/vickikbt/shared/data/network/ApiService.kt
@@ -4,23 +4,19 @@ import com.vickikbt.shared.data.network.models.CastDto
 import com.vickikbt.shared.data.network.models.MovieDetailsDto
 import com.vickikbt.shared.data.network.models.MovieResultsDto
 import com.vickikbt.shared.data.network.models.MovieVideoDto
+import com.vickikbt.shared.domain.utils.Constants.API_KEY
 import com.vickikbt.shared.domain.utils.Constants.STARTING_PAGE_INDEX
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
 
 class ApiService constructor(private val httpClient: HttpClient) {
 
     suspend fun fetchNowPlayingMovies(page: Int = STARTING_PAGE_INDEX): MovieResultsDto {
-        return httpClient.get<MovieResultsDto>(urlString = "movie/now_playing") {
+        return httpClient.get(urlString = "https://api.themoviedb.org/3/movie/now_playing?api_key=$API_KEY") {
             parameter("page", page)
-        }
-    }
-
-    suspend fun fetchPopularMovies(page: Int = STARTING_PAGE_INDEX): MovieResultsDto {
-        return httpClient.get<MovieResultsDto>(urlString = "movie/popular") {
-            parameter("page", page)
-        }
+        }.body<MovieResultsDto>()
     }
 
     suspend fun fetchTrendingMovies(
@@ -28,41 +24,47 @@ class ApiService constructor(private val httpClient: HttpClient) {
         timeWindow: String = "week",
         page: Int = STARTING_PAGE_INDEX
     ): MovieResultsDto {
-        return httpClient.get<MovieResultsDto>(urlString = "trending/$mediaType/$timeWindow") {
+        return httpClient.get(urlString = "https://api.themoviedb.org/3/trending/$mediaType/$timeWindow?api_key=$API_KEY") {
             parameter("page", page)
-        }
+        }.body<MovieResultsDto>()
+    }
+
+    suspend fun fetchPopularMovies(page: Int = STARTING_PAGE_INDEX): MovieResultsDto {
+        return httpClient.get(urlString = "https://api.themoviedb.org/3/movie/popular?api_key=$API_KEY") {
+            parameter("page", page)
+        }.body<MovieResultsDto>()
     }
 
     suspend fun fetchUpcomingMovies(page: Int = STARTING_PAGE_INDEX): MovieResultsDto {
-        return httpClient.get<MovieResultsDto>(urlString = "movie/upcoming") {
+        return httpClient.get(urlString = "https://api.themoviedb.org/3/movie/upcoming?api_key=$API_KEY") {
             parameter("page", page)
-        }
+        }.body<MovieResultsDto>()
     }
 
     suspend fun fetchMovieDetails(movieId: Int): MovieDetailsDto {
-        return httpClient.get<MovieDetailsDto>(urlString = "movie/$movieId") {
+        return httpClient.get(urlString = "https://api.themoviedb.org/3/movie/$movieId?api_key=$API_KEY") {
             parameter("movie_id", movieId)
-        }
+        }.body<MovieDetailsDto>()
     }
 
     suspend fun fetchMovieCast(movieId: Int): CastDto {
-        return httpClient.get<CastDto>(urlString = "movie/$movieId/credits") {
+        return httpClient.get(urlString = "https://api.themoviedb.org/3/movie/$movieId/credits?api_key=$API_KEY") {
             parameter("movie_id", movieId)
-        }
+        }.body<CastDto>()
     }
 
     suspend fun fetchMovieVideos(movieId: Int): MovieVideoDto {
-        return httpClient.get<MovieVideoDto>(urlString = "movie/$movieId/videos") {
+        return httpClient.get(urlString = "https://api.themoviedb.org/3/movie/$movieId/videos") {
             parameter("movie_id", movieId)
-        }
+        }.body<MovieVideoDto>()
     }
 
     suspend fun fetchSimilarMovies(
         movieId: Int,
         page: Int = STARTING_PAGE_INDEX
     ): MovieResultsDto {
-        return httpClient.get<MovieResultsDto>(urlString = "movie/$movieId/similar") {
+        return httpClient.get(urlString = "https://api.themoviedb.org/3/movie/$movieId/similar?api_key=$API_KEY") {
             parameter("page", page)
-        }
+        }.body<MovieResultsDto>()
     }
 }
