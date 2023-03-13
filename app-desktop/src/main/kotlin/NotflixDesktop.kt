1a2771f329373445cc814a3f401664b701f6ff02
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2023-03-14 00:30:58 +0300

    Refactor desktopApp module to app-desktop

--- app-desktop/src/main/kotlin/NotflixDesktop.kt
+++ app-desktop/src/main/kotlin/NotflixDesktop.kt
@@ -6,7 +6,7 @@ import ui.screens.main.MainScreen
 lateinit var koin: Koin
 
 fun main() {
-    koin = initKoin(enableNetworkLogs = false).koin
+    koin = initKoin(enableNetworkLogs = true).koin
 
     return application {
         MainScreen(applicationScope = this)
