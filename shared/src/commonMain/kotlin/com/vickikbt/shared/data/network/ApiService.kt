44b641201ecaac25147b4a6893bb0a01cce910e9
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2023-03-13 17:43:58 +0300

    Removed base url from url string

--- shared/src/commonMain/kotlin/com/vickikbt/shared/data/network/ApiService.kt
+++ shared/src/commonMain/kotlin/com/vickikbt/shared/data/network/ApiService.kt
@@ -4,7 +4,6 @@ import com.vickikbt.shared.data.network.models.CastDto
 import com.vickikbt.shared.data.network.models.MovieDetailsDto
 import com.vickikbt.shared.data.network.models.MovieResultsDto
 import com.vickikbt.shared.data.network.models.MovieVideoDto
-import com.vickikbt.shared.domain.utils.Constants.API_KEY
 import com.vickikbt.shared.domain.utils.Constants.STARTING_PAGE_INDEX
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
@@ -14,7 +13,7 @@ import io.ktor.client.request.parameter
 class ApiService constructor(private val httpClient: HttpClient) {
 
     suspend fun fetchNowPlayingMovies(page: Int = STARTING_PAGE_INDEX): MovieResultsDto {
-        return httpClient.get(urlString = "https://api.themoviedb.org/3/movie/now_playing?api_key=$API_KEY") {
+        return httpClient.get(urlString = "movie/now_playing") {
             parameter("page", page)
         }.body<MovieResultsDto>()
     }
@@ -24,37 +23,37 @@ class ApiService constructor(private val httpClient: HttpClient) {
         timeWindow: String = "week",
         page: Int = STARTING_PAGE_INDEX
     ): MovieResultsDto {
-        return httpClient.get(urlString = "https://api.themoviedb.org/3/trending/$mediaType/$timeWindow?api_key=$API_KEY") {
+        return httpClient.get(urlString = "trending/$mediaType/$timeWindow") {
             parameter("page", page)
         }.body<MovieResultsDto>()
     }
 
     suspend fun fetchPopularMovies(page: Int = STARTING_PAGE_INDEX): MovieResultsDto {
-        return httpClient.get(urlString = "https://api.themoviedb.org/3/movie/popular?api_key=$API_KEY") {
+        return httpClient.get(urlString = "movie/popular") {
             parameter("page", page)
         }.body<MovieResultsDto>()
     }
 
     suspend fun fetchUpcomingMovies(page: Int = STARTING_PAGE_INDEX): MovieResultsDto {
-        return httpClient.get(urlString = "https://api.themoviedb.org/3/movie/upcoming?api_key=$API_KEY") {
+        return httpClient.get(urlString = "movie/upcoming") {
             parameter("page", page)
         }.body<MovieResultsDto>()
     }
 
     suspend fun fetchMovieDetails(movieId: Int): MovieDetailsDto {
-        return httpClient.get(urlString = "https://api.themoviedb.org/3/movie/$movieId?api_key=$API_KEY") {
+        return httpClient.get(urlString = "movie/$movieId") {
             parameter("movie_id", movieId)
         }.body<MovieDetailsDto>()
     }
 
     suspend fun fetchMovieCast(movieId: Int): CastDto {
-        return httpClient.get(urlString = "https://api.themoviedb.org/3/movie/$movieId/credits?api_key=$API_KEY") {
+        return httpClient.get(urlString = "movie/$movieId/credits") {
             parameter("movie_id", movieId)
         }.body<CastDto>()
     }
 
     suspend fun fetchMovieVideos(movieId: Int): MovieVideoDto {
-        return httpClient.get(urlString = "https://api.themoviedb.org/3/movie/$movieId/videos") {
+        return httpClient.get(urlString = "movie/$movieId/videos") {
             parameter("movie_id", movieId)
         }.body<MovieVideoDto>()
     }
@@ -63,7 +62,7 @@ class ApiService constructor(private val httpClient: HttpClient) {
         movieId: Int,
         page: Int = STARTING_PAGE_INDEX
     ): MovieResultsDto {
-        return httpClient.get(urlString = "https://api.themoviedb.org/3/movie/$movieId/similar?api_key=$API_KEY") {
+        return httpClient.get(urlString = "movie/$movieId/similar") {
             parameter("page", page)
         }.body<MovieResultsDto>()
     }
