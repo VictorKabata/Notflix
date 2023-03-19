56aeca314dc1ca61e7bfb92fd075f43c5a689bd8
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2023-03-19 18:08:27 +0300

    Removed unused plugins

--- buildSrc/src/main/kotlin/Plugins.kt
+++ buildSrc/src/main/kotlin/Plugins.kt
@@ -3,25 +3,5 @@ object Plugins {
     const val detekt = "io.gitlab.arturbosch.detekt"
     const val gradleVersionUpdates = "com.github.ben-manes.versions"
     const val kotlinxTestResource="com.goncalossilva.resources"
-
-    const val androidApplication = "com.android.application"
-    const val android = "android"
-
-    const val crashlytics = "com.google.firebase.crashlytics"
-    const val appDistribution = "com.google.firebase.appdistribution"
-    const val googleService = "com.google.gms.google-services"
-    const val performance="com.google.firebase.firebase-perf"
-
-    const val multiplatform = "multiplatform"
-    const val androidLibrary = "com.android.library"
-    const val kotlinXSerialization = "plugin.serialization"
-    const val sqlDelight = "com.squareup.sqldelight"
-    const val nativeCoroutines = "com.rickclephas.kmp.nativecoroutines"
-    const val buildKonfig="com.codingfeline.buildkonfig"
     const val kover = "org.jetbrains.kotlinx.kover"
-
-    const val jvm = "jvm"
-    const val compose = "org.jetbrains.compose"
-
-    const val nativeCocoaPods = "native.cocoapods"
 }
