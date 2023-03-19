8f2506d7b26a494831f280d57e2d1ea5c9630d47
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2023-03-20 00:14:43 +0300

    Prefer settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         mavenCentral()
         google()
