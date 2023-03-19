89f970d1656193476d9bbc8f5d1b6ee5ae5a3c25
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2023-03-19 18:22:19 +0300

    Removed versions from buildSrc

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,3 +1,11 @@
+object Plugins {
+    const val ktLint = "org.jlleitschuh.gradle.ktlint"
+    const val detekt = "io.gitlab.arturbosch.detekt"
+    const val gradleVersionUpdates = "com.github.ben-manes.versions"
+    const val kotlinxTestResource="com.goncalossilva.resources"
+    const val kover = "org.jetbrains.kotlinx.kover"
+}
+
 object AndroidSdk {
     const val appName = "Notflix"
 
@@ -14,3 +22,11 @@ object AndroidSdk {
 
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 }
+
+object BuildModules {
+    const val shared = ":shared"
+    const val androidApp = ":androidApp"
+    const val iOSApp = ":iOSApp"
+    const val desktopApp = ":desktopApp"
+}
+
--- buildSrc/src/main/kotlin/Plugins.kt
+++
@@ -1,7 +0,0 @@
-object Plugins {
-    const val ktLint = "org.jlleitschuh.gradle.ktlint"
-    const val detekt = "io.gitlab.arturbosch.detekt"
-    const val gradleVersionUpdates = "com.github.ben-manes.versions"
-    const val kotlinxTestResource="com.goncalossilva.resources"
-    const val kover = "org.jetbrains.kotlinx.kover"
-}
