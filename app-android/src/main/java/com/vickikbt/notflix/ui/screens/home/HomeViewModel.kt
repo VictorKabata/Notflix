613aad720942a18d3a13a0c2a56fdaba30b7819e
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2023-03-21 14:21:00 +0300

    Migrate home viewmodel to use UI state

--- app-android/src/main/java/com/vickikbt/notflix/ui/screens/home/HomeViewModel.kt
+++ app-android/src/main/java/com/vickikbt/notflix/ui/screens/home/HomeViewModel.kt
@@ -2,28 +2,17 @@ package com.vickikbt.notflix.ui.screens.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.vickikbt.shared.domain.models.Movie
 import com.vickikbt.shared.domain.repositories.MoviesRepository
+import com.vickikbt.shared.utils.HomeUiStates
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class HomeViewModel constructor(private val moviesRepository: MoviesRepository) : ViewModel() {
 
-    private val _nowPlayingMovies = MutableStateFlow<List<Movie>?>(emptyList())
-    val nowPlayingMovies get() = _nowPlayingMovies.asStateFlow()
-
-    private val _trendingMovies = MutableStateFlow<List<Movie>?>(emptyList())
-    val trendingMovies get() = _trendingMovies.asStateFlow()
-
-    private val _popularMovies = MutableStateFlow<List<Movie>?>(emptyList())
-    val popularMovies get() = _popularMovies.asStateFlow()
-
-    private val _upcomingMovies = MutableStateFlow<List<Movie>?>(emptyList())
-    val upcomingMovies get() = _upcomingMovies.asStateFlow()
-
-    private val _error = MutableStateFlow<String?>(null)
-    val error get() = _error.asStateFlow()
+    private val _homeUiState = MutableStateFlow(HomeUiStates(isLoading = true))
+    val homeUiState = _homeUiState.asStateFlow()
 
     init {
         fetchNowPlayingMovies()
@@ -34,53 +23,41 @@ class HomeViewModel constructor(private val moviesRepository: MoviesRepository) 
 
     private fun fetchNowPlayingMovies() = viewModelScope.launch {
         moviesRepository.fetchNowPlayingMovies().collect { moviesResult ->
-            moviesResult.onSuccess {
-                _nowPlayingMovies.value = it
-            }.onFailure {
-                _error.value = it.message
+            moviesResult.onSuccess { movies ->
+                _homeUiState.update { it.copy(nowPlayingMovies = movies, isLoading = false) }
+            }.onFailure { error ->
+                _homeUiState.update { it.copy(error = error.localizedMessage, isLoading = false) }
             }
         }
     }
 
     private fun fetchTrendingMovies() = viewModelScope.launch {
-        try {
-            moviesRepository.fetchTrendingMovies().collect { moviesResult ->
-                moviesResult.onSuccess {
-                    _trendingMovies.value = it
-                }.onFailure {
-                    _error.value = it.message
-                }
+        moviesRepository.fetchTrendingMovies().collect { moviesResult ->
+            moviesResult.onSuccess { movies ->
+                _homeUiState.update { it.copy(trendingMovies = movies, isLoading = false) }
+            }.onFailure { error ->
+                _homeUiState.update { it.copy(error = error.localizedMessage, isLoading = false) }
             }
-        } catch (e: Exception) {
-            _error.value = e.message
         }
     }
 
     private fun fetchPopularMovies() = viewModelScope.launch {
-        try {
-            moviesRepository.fetchPopularMovies().collect { moviesResult ->
-                moviesResult.onSuccess {
-                    _popularMovies.value = it
-                }.onFailure {
-                    _error.value = it.message
-                }
+        moviesRepository.fetchPopularMovies().collect { moviesResult ->
+            moviesResult.onSuccess { movies ->
+                _homeUiState.update { it.copy(popularMovies = movies, isLoading = false) }
+            }.onFailure { error ->
+                _homeUiState.update { it.copy(error = error.localizedMessage, isLoading = false) }
             }
-        } catch (e: Exception) {
-            _error.value = e.message
         }
     }
 
     private fun fetchUpcomingMovies() = viewModelScope.launch {
-        try {
-            moviesRepository.fetchUpcomingMovies().collect { moviesResult ->
-                moviesResult.onSuccess {
-                    _upcomingMovies.value = it
-                }.onFailure {
-                    _error.value = it.message
-                }
+        moviesRepository.fetchUpcomingMovies().collect { moviesResult ->
+            moviesResult.onSuccess { movies ->
+                _homeUiState.update { it.copy(upcomingMovies = movies, isLoading = false) }
+            }.onFailure { error ->
+                _homeUiState.update { it.copy(error = error.localizedMessage, isLoading = false) }
             }
-        } catch (e: Exception) {
-            _error.value = e.message
         }
     }
 }
