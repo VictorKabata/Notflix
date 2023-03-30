6a83e80c796b5015a9dad0b11023e4d77b01b3af
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2023-03-30 16:53:26 +0300

    Moved test packages

--- app-android/src/test/java/com/vickikbt/notflix/ui/screens/details/DetailsViewModelTest.kt
+++ app-android/src/test/java/com/vickikbt/notflix/ui/screens/details/DetailsViewModelTest.kt
@@ -20,8 +20,10 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
+@Config(manifest=Config.NONE)
 class DetailsViewModelTest {
 
     @get:Rule
--- app-android/src/test/java/com/vickikbt/notflix/ui/screens/home/HomeViewModelTest.kt
+++ app-android/src/test/java/com/vickikbt/notflix/ui/screens/home/HomeViewModelTest.kt
@@ -17,8 +17,10 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
+@Config(manifest=Config.NONE)
 class HomeViewModelTest {
 
     @get:Rule
