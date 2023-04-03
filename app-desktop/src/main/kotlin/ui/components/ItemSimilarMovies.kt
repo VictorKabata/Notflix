00ed35797cf302f16345d9f7d5cbc878deea5040
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2023-04-03 13:31:55 +0300

    Updated item similar movies component to use kamel image loader

--- app-desktop/src/main/kotlin/ui/components/ItemSimilarMovies.kt
+++ app-desktop/src/main/kotlin/ui/components/ItemSimilarMovies.kt
@@ -3,7 +3,9 @@ package ui.components
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
@@ -11,6 +13,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
@@ -18,13 +21,22 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.vickikbt.shared.domain.models.Movie
+import io.kamel.image.KamelImage
+import io.kamel.image.lazyPainterResource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import utils.AsyncImage
 import utils.loadImage
 import utils.loadImageBitmap
 
 @Composable
 fun ItemSimilarMovies(movie: Movie) {
+
     val imageUrl = movie.posterPath?.loadImage()
+
+    val painterResource = lazyPainterResource(imageUrl ?: "") {
+        coroutineContext = Job() + Dispatchers.IO
+    }
 
     Column {
         Card(
@@ -33,13 +45,13 @@ fun ItemSimilarMovies(movie: Movie) {
                 .height(220.dp),
             shape = RoundedCornerShape(4.dp)
         ) {
-            AsyncImage(
+            KamelImage(
                 modifier = Modifier.fillMaxSize(),
-                load = { loadImageBitmap(imageUrl) },
-                painterFor = { remember { BitmapPainter(it) } },
+                resource = painterResource,
                 contentDescription = "Movie poster",
                 contentScale = ContentScale.Crop
             )
+
         }
 
         Text(
