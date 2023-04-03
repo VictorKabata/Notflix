38d93e55699e8358bb1530a11b5252c91b3ac14c
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2023-04-03 12:57:36 +0300

    Updated now playing to use kamel image loader

--- app-desktop/src/main/kotlin/ui/components/ItemNowPlaying.kt
+++ app-desktop/src/main/kotlin/ui/components/ItemNowPlaying.kt
@@ -4,31 +4,24 @@ import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.onPointerEvent
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.vickikbt.shared.domain.models.Movie
-import utils.AsyncImage
+import io.kamel.image.KamelImage
+import io.kamel.image.lazyPainterResource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import utils.loadImage
-import utils.loadImageBitmap
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
@@ -41,7 +34,11 @@ fun ItemNowPlayingMovies(
 
     val boxWidth by animateDpAsState(targetValue = if (isHovered) 400.dp else 200.dp)
 
-    val imageUrl = (if (isHovered) movie.backdropPath else movie.posterPath)?.loadImage()
+    val imageUrl = movie.posterPath?.loadImage()
+
+    val painterResource = lazyPainterResource(imageUrl ?: "") {
+        coroutineContext = Job() + Dispatchers.IO
+    }
 
     Box(
         modifier = modifier.width(boxWidth)
@@ -49,16 +46,15 @@ fun ItemNowPlayingMovies(
             .onPointerEvent(PointerEventType.Enter) { isHovered = true }
             .onPointerEvent(PointerEventType.Exit) { isHovered = false },
     ) {
-        AsyncImage(
+        KamelImage(
             modifier = Modifier.fillMaxSize(),
-            load = { loadImageBitmap(imageUrl) },
-            painterFor = { remember { BitmapPainter(it) } },
+            resource = painterResource,
             contentDescription = "Movie Backdrop Poster",
             contentScale = ContentScale.Crop
         )
 
         //region Movie Title
-        if (isHovered) {
+        /*if (isHovered) {
             Text(
                 modifier = Modifier.padding(horizontal = 8.dp, vertical = 16.dp)
                     .align(Alignment.BottomStart),
@@ -70,7 +66,7 @@ fun ItemNowPlayingMovies(
                 textAlign = TextAlign.Start,
                 color = MaterialTheme.colors.onSurface
             )
-        }
+        }*/
         //endregion
     }
 }
