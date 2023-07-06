623cdda85e17b0948e4a903481a1053b4e539a1a
Victor Kabata <victorbro14@gmail.com>
2023-07-06 16:33:43 +0300

    Fixed tests

--- shared/src/iOSMain/kotlin/com/vickikbt/shared/di/Module.kt
+++ shared/src/iOSMain/kotlin/com/vickikbt/shared/di/Module.kt
@@ -1,14 +1,12 @@
 package com.vickikbt.shared.di
 
-import com.vickikbt.shared.domain.utils.DatabaseDriverFactory
 import com.vickikbt.shared.domain.utils.MultiplatformSettingsWrapper
-import io.ktor.client.engine.ios.Ios
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 actual fun platformModule(): Module = module {
-    single { DatabaseDriverFactory() }
+    // single { DatabaseDriverFactory() }
 
     single { MultiplatformSettingsWrapper().createSettings() }
-    single { Ios.create() }
+    // single { Ios.create() }
 }
