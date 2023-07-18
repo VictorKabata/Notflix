4e64bfefc398a1942a94b5d2f42ecd6d4bfea35c
Victor Kabata <victorbro14@gmail.com>
2023-07-18 18:10:33 +0300

    Change compose mmp from 1.4.1 to 1.4.0

--- appiOS/appiOS/ContentView.swift
+++ appiOS/appiOS/ContentView.swift
@@ -9,8 +9,8 @@ struct ContentView: View {
 	}
 }
 
-struct ContentView_Previews: PreviewProvider {
-	static var previews: some View {
-		ContentView()
-	}
-}
+//struct ContentView_Previews: PreviewProvider {
+//	static var previews: some View {
+//		ContentView()
+//	}
+//}
