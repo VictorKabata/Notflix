c044129855d2d2979c2c1c116b79b92da4016f84
Victor Kabata <victorbro14@gmail.com>
2023-07-19 16:33:19 +0300

    Created appiOS

---
+++ appiOS/Notflix/NotflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotflixApp.swift
+//  Notflix
+//
+//  Created by Victor Kabata on 19/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct NotflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
