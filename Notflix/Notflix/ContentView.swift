03b4618e747c8d68cd3e637b43ede0d1bd7323a2
Victor Kabata <victorbro14@gmail.com>
2023-07-21 12:47:03 +0300

    Created new Notflix iOS app

---
+++ Notflix/Notflix/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Notflix
+//
+//  Created by Victor Kabata on 21/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
---
+++ Notflix/Notflix/NotflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotflixApp.swift
+//  Notflix
+//
+//  Created by Victor Kabata on 21/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct NotflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
