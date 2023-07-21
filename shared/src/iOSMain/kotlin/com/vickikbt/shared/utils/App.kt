5d83d9a85d74aa65b089d22c2871a3f298af72a0
Victor Kabata <victorbro14@gmail.com>
2023-07-21 14:27:04 +0300

    Deleted iosMain

--- shared/src/iOSMain/kotlin/com/vickikbt/shared/utils/App.kt
+++
@@ -1,8 +0,0 @@
-package com.vickikbt.shared.utils
-
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.ui.window.ComposeUIViewController
-import com.vickikbt.shared.presentation.ui.screens.home.HomeScreen
-
-@OptIn(ExperimentalMaterialApi::class)
-fun MainViewController() = ComposeUIViewController { HomeScreen() }
