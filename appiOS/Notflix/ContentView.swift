41cb1e2a5c7818a184786b38297c85a461c6e971
Victor Kabata <victorbro14@gmail.com>
2023-07-25 00:08:36 +0300

    Set swift UI view

--- appiOS/Notflix/ContentView.swift
+++ appiOS/Notflix/ContentView.swift
@@ -9,10 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Hello, world!")
-        }
-        .padding()
+        ComposeView()
     }
 }
 
