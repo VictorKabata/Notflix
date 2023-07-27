a4083471db5cec2b5f933ae6425e06c52221e35d
Victor Kabata <victorbro14@gmail.com>
2023-07-27 12:42:30 +0300

    Created favorites screen

--- shared/src/commonMain/kotlin/com/vickikbt/shared/presentation/ui/screens/favorites/FavoritesScreen.kt
+++ shared/src/commonMain/kotlin/com/vickikbt/shared/presentation/ui/screens/favorites/FavoritesScreen.kt
@@ -1,7 +1,28 @@
 package com.vickikbt.shared.presentation.ui.screens.favorites
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import com.vickikbt.shared.presentation.ui.theme.TextPrimary
 
 @Composable
 fun FavoritesScreen() {
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Text(
+            modifier = Modifier.align(Alignment.Center),
+            text = "Under\nDevelopment!",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            color = TextPrimary
+        )
+    }
+
 }
