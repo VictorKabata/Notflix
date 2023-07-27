ebf331060c1afd916b29373afb69d1c6c23d838e
Victor Kabata <victorbro14@gmail.com>
2023-07-27 15:49:34 +0300

    Update FavoritesScreen.kt

--- shared/src/commonMain/kotlin/com/vickikbt/shared/presentation/ui/screens/favorites/FavoritesScreen.kt
+++ shared/src/commonMain/kotlin/com/vickikbt/shared/presentation/ui/screens/favorites/FavoritesScreen.kt
@@ -2,6 +2,9 @@ package com.vickikbt.shared.presentation.ui.screens.favorites
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -9,20 +12,23 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
-import com.vickikbt.shared.presentation.ui.theme.TextPrimary
+import com.vickikbt.shared.presentation.ui.components.appbars.AppBar
 
 @Composable
 fun FavoritesScreen() {
-
-    Box(modifier = Modifier.fillMaxSize()) {
-        Text(
-            modifier = Modifier.align(Alignment.Center),
-            text = "Under\nDevelopment!",
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center,
-            color = TextPrimary
-        )
+    Scaffold(topBar = { AppBar("Favorites") }) {
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.surface) {
+            Box(modifier = Modifier.fillMaxSize()) {
+                Text(
+                    modifier = Modifier.align(Alignment.Center),
+                    text = "Under\nDevelopment!",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    color = MaterialTheme.colors.onSurface
+                )
+            }
+        }
     }
 
 }
