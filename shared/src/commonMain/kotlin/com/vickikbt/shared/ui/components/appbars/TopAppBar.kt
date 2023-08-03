d75f9b630fa3809f7b97e2b2842ba94cd647f359
Victor Kabata <victorbro14@gmail.com>
2023-08-03 12:58:54 +0300

    Loading appBar

--- shared/src/commonMain/kotlin/com/vickikbt/shared/ui/components/appbars/TopAppBar.kt
+++ shared/src/commonMain/kotlin/com/vickikbt/shared/ui/components/appbars/TopAppBar.kt
@@ -1,36 +1,25 @@
 package com.vickikbt.shared.ui.components.appbars
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AppBar(title: String, navigationIcon: Painter? = null, onClickNavigationIcon: () -> Unit = {}) {
-    MediumTopAppBar(
+fun AppBar(title: String) {
+    TopAppBar(
         colors = TopAppBarDefaults.mediumTopAppBarColors(MaterialTheme.colorScheme.surface),
-        title = {}
-    )
-    /*{
-            if (navigationIcon != null) {
-                IconButton(
-                    modifier = Modifier
-                        .size(40.dp),
-                    onClick = { onClickNavigationIcon() }
-                ) {
-                    Icon(
-                        painter = navigationIcon,
-                        contentDescription = "Navigation icon",
-                        tint = MaterialTheme.colorScheme.onSurface
-                    )
-                }
-            }
-
+        title = {
             Text(
-                modifier = Modifier.padding(horizontal = 24.dp, vertical = 8.dp),
+                modifier = Modifier.padding(horizontal = 20.dp, vertical = 8.dp),
                 text = title,
                 color = MaterialTheme.colorScheme.onSurface,
                 style = MaterialTheme.typography.titleMedium,
@@ -38,5 +27,6 @@ fun AppBar(title: String, navigationIcon: Painter? = null, onClickNavigationIcon
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis
             )
-        }*/
+        }
+    )
 }
--- shared/src/commonMain/kotlin/com/vickikbt/shared/ui/screens/main/MainScreen.kt
+++ shared/src/commonMain/kotlin/com/vickikbt/shared/ui/screens/main/MainScreen.kt
@@ -1,14 +1,16 @@
 package com.vickikbt.shared.ui.screens.main
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import com.vickikbt.shared.ui.components.BottomNavBar
+import androidx.compose.ui.Modifier
 import com.vickikbt.shared.presentation.ui.navigation.NavigationItem
 import com.vickikbt.shared.presentation.ui.screens.main.MainViewModel
+import com.vickikbt.shared.ui.components.BottomNavBar
 import com.vickikbt.shared.ui.navigation.Navigation
 import com.vickikbt.shared.ui.theme.NotflixTheme
 import moe.tlaster.precompose.navigation.rememberNavigator
@@ -23,26 +25,26 @@ fun MainScreen(viewModel: MainViewModel = koinInject()) {
     val isDarkTheme = appUiState.selectedTheme != 0
 
     NotflixTheme(darkTheme = isDarkTheme) {
-        Surface(color = MaterialTheme.colorScheme.surface) {
 
-            val navigator = rememberNavigator()
+        val navigator = rememberNavigator()
 
-            val topLevelDestinations = listOf(
-                NavigationItem.Home,
-                NavigationItem.Favorites,
-                NavigationItem.Settings
-            )
+        val topLevelDestinations = listOf(
+            NavigationItem.Home,
+            NavigationItem.Favorites,
+            NavigationItem.Settings
+        )
 
-            val isTopLevelDestination =
-                navigator.currentEntry.collectAsState(null).value?.route?.route in topLevelDestinations.map { it.route }
+        val isTopLevelDestination =
+            navigator.currentEntry.collectAsState(null).value?.route?.route in topLevelDestinations.map { it.route }
 
-            Scaffold(
-                bottomBar = {
-                    if (isTopLevelDestination) {
-                        BottomNavBar(bottomNavItems = topLevelDestinations, navigator = navigator)
-                    }
+        Scaffold(
+            bottomBar = {
+                if (isTopLevelDestination) {
+                    BottomNavBar(bottomNavItems = topLevelDestinations, navigator = navigator)
                 }
-            ) {
+            }
+        ) {
+            Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.surface) {
                 Navigation(navigator = navigator)
             }
         }
