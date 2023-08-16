d1203357d327b00eacb7b8edb0ce0c94b4bcb2a5
Victor Kabata <victorbro14@gmail.com>
2023-08-16 15:13:21 +0300

    Set up notflix desktop app module

---
+++ appDesktop/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    alias(libs.plugins.jvm)
+    alias(libs.plugins.compose)
+}
+
+dependencies {
+    implementation(project(":shared"))
+
+    implementation(compose.desktop.currentOs)
+
+}
+
+compose.desktop {
+    application {
+        mainClass = "NotflixApplicationKt"
+    }
+}
