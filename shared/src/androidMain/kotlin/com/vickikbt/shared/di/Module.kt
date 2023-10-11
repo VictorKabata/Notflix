4b1df4b577b9ef7fd5901309e78b73e7eb5e87a9
Victor Kabata <victorbro14@gmail.com>
2023-10-11 19:48:27 +0300

    Remove context from mulitplatform settings di singleton

--- shared/src/androidMain/kotlin/com/vickikbt/shared/di/Module.kt
+++ shared/src/androidMain/kotlin/com/vickikbt/shared/di/Module.kt
@@ -1,10 +1,9 @@
 package com.vickikbt.shared.di
 
-import com.vickikbt.shared.utils.CustomImageLoader
 import com.vickikbt.shared.utils.MultiplatformSettingsWrapper
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 actual fun platformModule(): Module = module {
-    single { MultiplatformSettingsWrapper(context = get()).createSettings() }
+    single { MultiplatformSettingsWrapper().createSettings() }
 }
