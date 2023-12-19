2e219d0c279ade681287123f8416b544ae066bc1
Victor Kabata <victorbro14@gmail.com>
2023-12-19 15:05:21 +0300

    Returning favorite movies as flow list

--- shared/src/commonMain/kotlin/com/vickbt/shared/data/cache/sqldelight/daos/FavoriteMovieDao.kt
+++ shared/src/commonMain/kotlin/com/vickbt/shared/data/cache/sqldelight/daos/FavoriteMovieDao.kt
@@ -1,8 +1,12 @@
 package com.vickbt.shared.data.cache.sqldelight.daos
 
+import app.cash.sqldelight.coroutines.asFlow
+import app.cash.sqldelight.coroutines.mapToList
 import com.vickbt.shared.AppDatabase
 import com.vickbt.shared.domain.models.MovieDetails
 import com.vickbt.shared.utils.DatabaseDriverFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 
 class FavoriteMovieDao(private val databaseDriverFactory: DatabaseDriverFactory) {
 
@@ -31,7 +35,7 @@ class FavoriteMovieDao(private val databaseDriverFactory: DatabaseDriverFactory)
     }
 
     /**Returns a list of all movie details in FavoriteMovie table*/
-    fun getAllFavoriteMovies() = dbQuery.getAllFavoriteMovies()
+    fun getAllFavoriteMovies() = dbQuery.getAllFavoriteMovies().asFlow().mapToList(Dispatchers.IO)
 
     /**Delete favorite movie details based on its ID*/
     fun deleteFavouriteMovie(id: Int) = dbQuery.deleteFavoriteMovie(id = id.toLong())
