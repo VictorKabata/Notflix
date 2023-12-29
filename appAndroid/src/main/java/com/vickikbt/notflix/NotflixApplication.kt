b6039014afc985f30879802b0ec8fbf3c0fcdf40
Victor Kabata <victorbro14@gmail.com>
2023-12-29 13:24:45 +0300

    Update koin libs

--- appAndroid/src/main/java/com/vickikbt/notflix/NotflixApplication.kt
+++ appAndroid/src/main/java/com/vickikbt/notflix/NotflixApplication.kt
@@ -3,7 +3,7 @@ package com.vickikbt.notflix
 import android.app.Application
 import com.vickikbt.shared.di.initKoin
 import com.vickikbt.shared.utils.ContextUtils
-import org.koin.android.BuildConfig
+import io.github.aakira.napier.BuildConfig
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.logger.Level
