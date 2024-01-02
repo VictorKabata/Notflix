407efd8f60e35755356d84d264b005efc99d5b7b
Victor Kabata <victorbro14@gmail.com>
2024-01-02 22:44:54 +0300

    Removed deprecation warning in iOSMain

--- shared/src/iosMain/kotlin/com/vickbt/shared/utils/MainViewController.kt
+++ shared/src/iosMain/kotlin/com/vickbt/shared/utils/MainViewController.kt
@@ -1,6 +1,7 @@
 package com.vickbt.shared.utils
 
+import androidx.compose.ui.window.ComposeUIViewController
 import com.vickbt.shared.ui.screens.main.MainScreen
 import moe.tlaster.precompose.PreComposeApplication
 
-fun MainViewController() = PreComposeApplication { MainScreen() }
+fun MainViewController() = ComposeUIViewController { MainScreen() }
