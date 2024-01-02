e29039bd4e26c3258cd36820cedf1a57fbfa0b46
Victor Kabata <victorbro14@gmail.com>
2024-01-02 22:55:08 +0300

    Linting

--- shared/src/iosMain/kotlin/com/vickbt/shared/utils/MainViewController.kt
+++ shared/src/iosMain/kotlin/com/vickbt/shared/utils/MainViewController.kt
@@ -2,6 +2,5 @@ package com.vickbt.shared.utils
 
 import androidx.compose.ui.window.ComposeUIViewController
 import com.vickbt.shared.ui.screens.main.MainScreen
-import moe.tlaster.precompose.PreComposeApplication
 
 fun MainViewController() = ComposeUIViewController { MainScreen() }
