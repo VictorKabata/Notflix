94b66f41fe2908ccfb8c62f7cf9987c095c33595
Victor Kabata <victorbro14@gmail.com>
2024-01-03 15:00:35 +0300

    Update settings repo with experimental annotation

--- shared/src/commonMain/kotlin/com/vickbt/shared/data/datasources/SettingsRepositoryImpl.kt
+++ shared/src/commonMain/kotlin/com/vickbt/shared/data/datasources/SettingsRepositoryImpl.kt
@@ -1,23 +1,25 @@
 package com.vickbt.shared.data.datasources
 
+import com.russhwolf.settings.ExperimentalSettingsApi
 import com.russhwolf.settings.ObservableSettings
-import com.russhwolf.settings.coroutines.getIntOrNullFlow
+import com.russhwolf.settings.coroutines.getIntFlow
 import com.vickbt.shared.domain.repositories.SettingsRepository
 import com.vickbt.shared.domain.utils.Constants.KEY_IMAGE_QUALITY
 import com.vickbt.shared.domain.utils.Constants.KEY_THEME
 import kotlinx.coroutines.flow.Flow
 
-class SettingsRepositoryImpl constructor(private val observableSettings: ObservableSettings) :
+@ExperimentalSettingsApi
+class SettingsRepositoryImpl(private val observableSettings: ObservableSettings) :
     SettingsRepository {
 
     override suspend fun savePreferenceSelection(key: String, selection: Int) =
         observableSettings.putInt(key = key, value = selection)
 
     override suspend fun getThemePreference(): Flow<Int?> {
-        return observableSettings.getIntOrNullFlow(key = KEY_THEME)
+        return observableSettings.getIntFlow(key = KEY_THEME, defaultValue = 0)
     }
 
     override suspend fun getImageQualityPreference(): Flow<Int?> {
-        return observableSettings.getIntOrNullFlow(key = KEY_IMAGE_QUALITY)
+        return observableSettings.getIntFlow(key = KEY_IMAGE_QUALITY, defaultValue = 0)
     }
 }
