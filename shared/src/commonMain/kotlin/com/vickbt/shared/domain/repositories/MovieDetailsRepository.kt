32366ac83e8d80e6999659794947c1b977961485
Victor Kabata <victorbro14@gmail.com>
2024-01-03 14:50:16 +0300

    Renamed NetworkResultState class to ResultState

--- shared/src/commonMain/kotlin/com/vickbt/shared/domain/repositories/MovieDetailsRepository.kt
+++ shared/src/commonMain/kotlin/com/vickbt/shared/domain/repositories/MovieDetailsRepository.kt
@@ -4,22 +4,22 @@ import com.vickbt.shared.domain.models.Cast
 import com.vickbt.shared.domain.models.Movie
 import com.vickbt.shared.domain.models.MovieDetails
 import com.vickbt.shared.domain.utils.Constants.STARTING_PAGE_INDEX
-import com.vickbt.shared.utils.NetworkResultState
+import com.vickbt.shared.utils.ResultState
 import kotlinx.coroutines.flow.Flow
 
 interface MovieDetailsRepository {
 
     /**Fetch movie details from network source*/
-    suspend fun fetchMovieDetails(movieId: Int): Flow<NetworkResultState<MovieDetails>>
+    suspend fun fetchMovieDetails(movieId: Int): Flow<ResultState<MovieDetails>>
 
     /**Fetch movie cast from network source*/
-    suspend fun fetchMovieCast(movieId: Int): Flow<NetworkResultState<Cast>>
+    suspend fun fetchMovieCast(movieId: Int): Flow<ResultState<Cast>>
 
     /** Fetches similar movies from network source*/
     suspend fun fetchSimilarMovies(
         movieId: Int,
         page: Int = STARTING_PAGE_INDEX
-    ): Flow<NetworkResultState<List<Movie>?>>
+    ): Flow<ResultState<List<Movie>?>>
 
     /**Save movie details to local cache*/
     suspend fun saveFavoriteMovie(movie: MovieDetails)
--- shared/src/commonMain/kotlin/com/vickbt/shared/domain/repositories/MoviesRepository.kt
+++ shared/src/commonMain/kotlin/com/vickbt/shared/domain/repositories/MoviesRepository.kt
@@ -2,26 +2,26 @@ package com.vickbt.shared.domain.repositories
 
 import com.vickbt.shared.domain.models.Movie
 import com.vickbt.shared.domain.utils.Constants.STARTING_PAGE_INDEX
-import com.vickbt.shared.utils.NetworkResultState
+import com.vickbt.shared.utils.ResultState
 import kotlinx.coroutines.flow.Flow
 
 interface MoviesRepository {
 
     /** Fetch Now Playing movies from data source*/
-    suspend fun fetchNowPlayingMovies(page: Int = STARTING_PAGE_INDEX): Flow<NetworkResultState<List<Movie>?>>
+    suspend fun fetchNowPlayingMovies(page: Int = STARTING_PAGE_INDEX): Flow<ResultState<List<Movie>?>>
 
     /** Fetch Trending movies from data source*/
     suspend fun fetchTrendingMovies(
         mediaType: String = "movie",
         timeWindow: String = "week",
         page: Int = STARTING_PAGE_INDEX
-    ): Flow<NetworkResultState<List<Movie>?>>
+    ): Flow<ResultState<List<Movie>?>>
 
     /** Fetch Popular movies from data source*/
-    suspend fun fetchPopularMovies(page: Int = STARTING_PAGE_INDEX): Flow<NetworkResultState<List<Movie>?>>
+    suspend fun fetchPopularMovies(page: Int = STARTING_PAGE_INDEX): Flow<ResultState<List<Movie>?>>
 
     /** Fetch Upcoming movies from data source*/
-    suspend fun fetchUpcomingMovies(page: Int = STARTING_PAGE_INDEX): Flow<NetworkResultState<List<Movie>?>>
+    suspend fun fetchUpcomingMovies(page: Int = STARTING_PAGE_INDEX): Flow<ResultState<List<Movie>?>>
 
     /** Get movies based on category from cache*/
     /*@Deprecated("Pending caching implementation")
@@ -31,5 +31,5 @@ interface MoviesRepository {
     suspend fun searchMovie(
         movieName: String,
         page: Int = STARTING_PAGE_INDEX
-    ): Flow<NetworkResultState<List<Movie>?>>
+    ): Flow<ResultState<List<Movie>?>>
 }
