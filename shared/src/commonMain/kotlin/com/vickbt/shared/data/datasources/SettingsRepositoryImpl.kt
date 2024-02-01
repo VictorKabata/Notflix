dee5ec0a41a1aa06d75b05f692f49cd56c905d2a
Victor Kabata <victorbro14@gmail.com>
2024-02-01 17:02:59 +0300

    Updated multiplatform settings call to use IO dispatcher withContext

--- shared/src/commonMain/kotlin/com/vickbt/shared/data/datasources/SettingsRepositoryImpl.kt
+++ shared/src/commonMain/kotlin/com/vickbt/shared/data/datasources/SettingsRepositoryImpl.kt
@@ -6,20 +6,29 @@ import com.russhwolf.settings.coroutines.getIntFlow
 import com.vickbt.shared.domain.repositories.SettingsRepository
 import com.vickbt.shared.domain.utils.Constants.KEY_IMAGE_QUALITY
 import com.vickbt.shared.domain.utils.Constants.KEY_THEME
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
 
 @ExperimentalSettingsApi
 class SettingsRepositoryImpl(private val observableSettings: ObservableSettings) :
     SettingsRepository {
 
     override suspend fun savePreferenceSelection(key: String, selection: Int) =
-        observableSettings.putInt(key = key, value = selection)
+        withContext(Dispatchers.IO) {
+            observableSettings.putInt(key = key, value = selection)
+        }
 
     override suspend fun getThemePreference(): Flow<Int> {
-        return observableSettings.getIntFlow(key = KEY_THEME, defaultValue = 2)
+        return withContext(Dispatchers.IO) {
+            observableSettings.getIntFlow(key = KEY_THEME, defaultValue = 2)
+        }
     }
 
     override suspend fun getImageQualityPreference(): Flow<Int> {
-        return observableSettings.getIntFlow(key = KEY_IMAGE_QUALITY, defaultValue = 1)
+        return withContext(Dispatchers.IO) {
+            observableSettings.getIntFlow(key = KEY_IMAGE_QUALITY, defaultValue = 1)
+        }
     }
 }
