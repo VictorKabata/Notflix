de1158b34c84e475721c3b049ddb139694e90d8c
Victor Kabata <victorbro14@gmail.com>
2024-02-02 17:04:11 +0300

    Clean up in shared module

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -122,8 +122,6 @@ android {
     }
 
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
-    sourceSets["main"].res.srcDirs("src/androidMain/res")
-    sourceSets["main"].resources.srcDirs("src/commonMain/resources")
 }
 
 buildkonfig {
