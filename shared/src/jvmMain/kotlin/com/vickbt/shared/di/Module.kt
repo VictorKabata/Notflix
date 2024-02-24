73f40cc47cebd42913fd070c7ce389ee12d94761
Victor Kabata <victorbro14@gmail.com>
2024-02-25 01:07:41 +0300

    Removed unused database driver factory

--- shared/src/jvmMain/kotlin/com/vickbt/shared/di/Module.kt
+++ shared/src/jvmMain/kotlin/com/vickbt/shared/di/Module.kt
@@ -5,6 +5,5 @@ import org.koin.core.module.Module
 import org.koin.dsl.module
 
 actual fun platformModule(): Module = module {
-    single { DatabaseDriverFactory() }
     single { MultiplatformSettingsWrapper().createSettings() }
 }
