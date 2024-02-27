e19cd8c2799f037d1d7f1d03ba0b414879d8c01a
Victor Kabata <victorbro14@gmail.com>
2024-02-27 10:14:08 +0300

    Create VideoQualityText.kt

---
+++ shared/src/commonMain/kotlin/com/vickbt/shared/ui/components/VideoQualityText.kt
@@ -0,0 +1,35 @@
+package com.vickbt.shared.ui.components
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun VideoQualityText(
+    modifier: Modifier = Modifier,
+    quality: String,
+    shape: RoundedCornerShape = RoundedCornerShape(4.dp),
+    border: BorderStroke = BorderStroke(1.dp, Color.White),
+    textColor: Color = Color.White,
+    cardColors: CardColors = CardDefaults.cardColors(containerColor = Color.Transparent)
+) {
+    Card(modifier = modifier.padding(2.dp), shape = shape, border = border, colors = cardColors) {
+        Text(
+            modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp),
+            text = quality,
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Bold,
+            color = textColor
+        )
+    }
+}
