f1eeceeadceb35fe2b6ad2a96fc7c87e66b37f05
Victor Kabata <victorbro14@gmail.com>
2024-07-24 20:02:42 +0300

    Set up dataStore in settings repository

--- shared/src/commonMain/kotlin/com/vickbt/shared/data/datasources/SettingsRepositoryImpl.kt
+++ shared/src/commonMain/kotlin/com/vickbt/shared/data/datasources/SettingsRepositoryImpl.kt
@@ -1,25 +1,38 @@
 package com.vickbt.shared.data.datasources
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
 import com.russhwolf.settings.ExperimentalSettingsApi
 import com.russhwolf.settings.ObservableSettings
-import com.russhwolf.settings.coroutines.getIntFlow
 import com.vickbt.shared.domain.repositories.SettingsRepository
 import com.vickbt.shared.domain.utils.Constants.KEY_IMAGE_QUALITY
 import com.vickbt.shared.domain.utils.Constants.KEY_THEME
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 @ExperimentalSettingsApi
-class SettingsRepositoryImpl(private val observableSettings: ObservableSettings) :
-    SettingsRepository {
+class SettingsRepositoryImpl(
+    private val observableSettings: ObservableSettings,
+    private val dataStore: DataStore<Preferences>
+) : SettingsRepository {
 
-    override suspend fun savePreferenceSelection(key: String, selection: Int) =
-        observableSettings.putInt(key = key, value = selection)
+    override suspend fun savePreferenceSelection(key: String, selection: Int) {
+        dataStore.edit { preferences ->
+            preferences[intPreferencesKey(key)] = selection
+        }
+    }
 
     override suspend fun getThemePreference(): Flow<Int> {
-        return observableSettings.getIntFlow(key = KEY_THEME, defaultValue = 2)
+        return dataStore.data.map { preferences ->
+            preferences[intPreferencesKey(KEY_THEME)] ?: 2
+        }
     }
 
     override suspend fun getImageQualityPreference(): Flow<Int> {
-        return observableSettings.getIntFlow(key = KEY_IMAGE_QUALITY, defaultValue = 1)
+        return dataStore.data.map { preferences ->
+            preferences[intPreferencesKey(KEY_IMAGE_QUALITY)] ?: 1
+        }
     }
 }
