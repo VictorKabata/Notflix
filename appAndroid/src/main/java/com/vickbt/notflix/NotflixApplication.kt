2aa1edc399570ead38e146430fa8638f305d7926
Victor Kabata <victorbro14@gmail.com>
2024-07-26 08:49:19 +0300

    Removed context utils class

--- appAndroid/src/main/java/com/vickbt/notflix/NotflixApplication.kt
+++ appAndroid/src/main/java/com/vickbt/notflix/NotflixApplication.kt
@@ -2,7 +2,6 @@ package com.vickbt.notflix
 
 import android.app.Application
 import com.vickbt.shared.di.initKoin
-import com.vickbt.shared.utils.ContextUtils
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.logger.Level
@@ -11,8 +10,6 @@ class NotflixApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-
-        ContextUtils.setContext(context = this)
 
         initKoin {
             androidLogger(level = Level.NONE)
