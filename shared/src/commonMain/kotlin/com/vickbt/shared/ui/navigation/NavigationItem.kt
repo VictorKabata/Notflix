fca85258841da202e2280dc71c0f6d9bfd414a7c
Victor Kabata <victorbro14@gmail.com>
2024-08-24 21:48:04 +0300

    Added search item to navigation items

--- shared/src/commonMain/kotlin/com/vickbt/shared/ui/navigation/NavigationItem.kt
+++ shared/src/commonMain/kotlin/com/vickbt/shared/ui/navigation/NavigationItem.kt
@@ -3,6 +3,7 @@ package com.vickbt.shared.ui.navigation
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Favorite
 import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.ui.graphics.vector.ImageVector
 
@@ -14,6 +15,7 @@ sealed class NavigationItem(
 
     object Home : NavigationItem("/home", "Home", Icons.Rounded.Home)
     object Favorites : NavigationItem("/favorites", "Favorites", Icons.Rounded.Favorite)
+    object Search : NavigationItem("/search", "Search", Icons.Rounded.Search)
     object Settings : NavigationItem("/settings", "Settings", Icons.Rounded.Settings)
     object Details : NavigationItem("/details/{movieId}", "Details", null)
 }
