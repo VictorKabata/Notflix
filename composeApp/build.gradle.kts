b1849469f8b2b425f846be3b43709058dd8f51de
Victor Kabata <victorbro14@gmail.com>
2024-09-19 19:57:56 +0300

    Update android jdk version to 20 from 17

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -142,8 +142,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_20
+        targetCompatibility = JavaVersion.VERSION_20
     }
 
     buildFeatures {
