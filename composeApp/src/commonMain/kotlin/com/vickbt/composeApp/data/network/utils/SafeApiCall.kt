e1efe6e0cfb9166dab351cf0dedbe425298b4180
Victor Kabata <victorbro14@gmail.com>
2024-09-23 16:37:11 +0300

    Update SafeApiCall.kt

--- composeApp/src/commonMain/kotlin/com/vickbt/composeApp/data/network/utils/SafeApiCall.kt
+++ composeApp/src/commonMain/kotlin/com/vickbt/composeApp/data/network/utils/SafeApiCall.kt
@@ -4,30 +4,13 @@ import com.vickbt.composeApp.data.mappers.toDomain
 import com.vickbt.composeApp.data.network.models.ErrorResponseDto
 import com.vickbt.composeApp.domain.models.ErrorResponse
 import io.ktor.client.call.body
-import io.ktor.client.plugins.ClientRequestException
-import io.ktor.client.plugins.RedirectResponseException
-import io.ktor.client.plugins.ServerResponseException
 import io.ktor.client.statement.HttpResponse
-import io.ktor.util.network.UnresolvedAddressException
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 
-suspend fun <T : Any?> safeApiCall(apiCall: suspend () -> T): Result<T> {
-    return try {
-        Result.success(apiCall.invoke())
-    } catch (e: RedirectResponseException) {
-        val error = parseNetworkError(e.response.body())
-        Result.failure(exception = error)
-    } catch (e: ClientRequestException) {
-        val error = parseNetworkError(e.response.body())
-        Result.failure(exception = error)
-    } catch (e: ServerResponseException) {
-        val error = parseNetworkError(e.response.body())
-        Result.failure(exception = error)
-    } catch (e: UnresolvedAddressException) {
-        val error = parseNetworkError(exception = e)
-        Result.failure(exception = error)
-    } catch (e: Exception) {
-        val error = parseNetworkError(exception = e)
-        Result.failure(exception = error)
+suspend fun <T : Any?> safeApiCall(apiCall: suspend () -> T): Result<Flow<T>> {
+    return runCatching {
+        flowOf(apiCall.invoke())
     }
 }
 
