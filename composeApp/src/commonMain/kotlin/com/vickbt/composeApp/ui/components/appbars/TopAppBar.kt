2c08f4efe954aadf8e3dc6db36b6e219000bb5f1
Victor Kabata <victorbro14@gmail.com>
2024-11-22 17:39:31 +0300

    Added action to TopAppBar component

--- composeApp/src/commonMain/kotlin/com/vickbt/composeApp/ui/components/appbars/TopAppBar.kt
+++ composeApp/src/commonMain/kotlin/com/vickbt/composeApp/ui/components/appbars/TopAppBar.kt
@@ -1,7 +1,12 @@
 package com.vickbt.composeApp.ui.components.appbars
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -14,19 +19,27 @@ import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AppBar(title: String) {
+fun AppBar(title: String, onActionClicked: () -> Unit = {}) {
     TopAppBar(
-        colors = TopAppBarDefaults.mediumTopAppBarColors(MaterialTheme.colorScheme.surface),
+        colors = TopAppBarDefaults.mediumTopAppBarColors(MaterialTheme.colorScheme.background),
         title = {
             Text(
                 modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
                 text = title,
                 color = MaterialTheme.colorScheme.onSurface,
-                style = MaterialTheme.typography.titleMedium,
-                fontSize = 28.sp,
+                style = MaterialTheme.typography.titleMedium.copy(fontSize = 28.sp),
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis
             )
+        },
+        actions = {
+            IconButton(modifier = Modifier.padding(6.dp), onClick = { onActionClicked() }) {
+                Icon(
+                    modifier = Modifier.size(28.dp),
+                    imageVector = Icons.Rounded.Search,
+                    contentDescription = null
+                )
+            }
         }
     )
 }
